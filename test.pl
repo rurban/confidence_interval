0f0556f19c657ecb9991a776685a045a34198328
Reini Urban <rurban@cpanel.net>
2015-07-09 10:35:23 +0200

    fix the tests: match the pl and cmd numbers

--- test.pl
+++ test.pl
@@ -3,5 +3,6 @@ use strict;
 use confidence_interval;
 
 for my $i (1, 10, 100, 1000, 10000, 100000) {
+    printf "confidence_interval(%u, %u, %0.1f)\n",2*$i, 10*$i, 0.1;
     printf "%f\n", confidence_interval::wilson_lower_bound(2*$i, 10*$i, 0.1);
 }
